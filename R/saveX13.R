d9fe079e5f25c3bf246924229ca87aa1a452af61
daianemarcolino <daimmattos@gmail.com>
2018-02-16 11:12:40 -0200

    export csv files

--- R/saveX13.R
+++ R/saveX13.R
@@ -6,44 +6,18 @@
  
 saveX13 <- function(output, file = ""){
   
-  if(file == ""){ stop("Inserir nome do arquivo")}
+  if(file == ""){ stop("Insert the file name. No extensions are required.")}
 
-  ajustadas <- data.frame(date = as.Date(output$x_as), output$x_as)
-  fatores_totais <- data.frame(date = as.Date(output$fatores_totais), output$fatores_totais)
-  fatores_sazonais <- data.frame(date = as.Date(output$fatores_sazonais), output$fatores_sazonais)
-  fatores_calendario <- data.frame(date = as.Date(output$fatores_calendario), output$fatores_calendario)
-  
-  # Criar caminho para gravar series temporais Com Ajuste Sazonal
-  path.saida <- paste0("./saida/", titulo, "_cas.xlsx")
+  ajustadas <- data.frame(date = as.Date(output$xSA), output$xSA)
+  fatores_totais <- data.frame(date = as.Date(output$totalFactors), output$totalFactors)
+  fatores_sazonais <- data.frame(date = as.Date(output$seasonalFactors), output$seasonalFactors)
+  fatores_calendario <- data.frame(date = as.Date(output$calendarFactors), output$calendarFactors)
   
   # salvar serie com ajuste sazonal
-  write.csv2(ajustadas, file = paste0(file,"_sa"), row.names = F, na = "", fileEncoding = "utf-8")
-  write.csv2(fatores_totais, file = paste0(file,"_tf"), row.names = F, na = "", fileEncoding = "utf-8")
-  write.csv2(fatores_calendario, file = paste0(file,"_cf"), row.names = F, na = "", fileEncoding = "utf-8")
-  write.csv2(fatores_sazonais, file = paste0(file,"_sf"), row.names = F, na = "", fileEncoding = "utf-8")
- 
-  # salvar especificacoes do modelo para ajustes posteriores.
-  
-  nomes <- c("arima.model", "transform.function",
-             "regression.variables", "calendar.effects","outliers.estimated", "stability", 
-             "qs.original", "qs.original.corrected","qs.series.sa")
-  masc <- output$esp[[1]] == ""
-  output$esp[[1]][masc] <- " " 
-  
-  tabela <- data.frame(output$esp[[1]][nomes][1:9], stringsAsFactors = FALSE)
-  
-  if(n > 1)
-  {    
-    for(i in 2:length(output$esp))
-    {
-      masc <- output$esp[[i]] == ""
-      output$esp[[i]][masc] <- " " 
-      tabela <- rbind(tabela, output$esp[[i]][1:9])
-    }
-  }
-  tabela <- cbind(serie = colnames(output$xts), tabela)
-  
-  # salvar fatores calendario
-  write.csv2(tabela, file = paste0(file,"_espec"), row.names = F, na = "", fileEncoding = "utf-8")
+  write.csv2(ajustadas, file = paste0(file,"_seasonallyAdjusted.csv"), row.names = F, na = "", fileEncoding = "utf-8")
+  write.csv2(fatores_totais, file = paste0(file,"_totalFactors.csv"), row.names = F, na = "", fileEncoding = "utf-8")
+  write.csv2(fatores_calendario, file = paste0(file,"_calendarFactors.csv"), row.names = F, na = "", fileEncoding = "utf-8")
+  write.csv2(fatores_sazonais, file = paste0(file,"_seasonalFactors.csv"), row.names = F, na = "", fileEncoding = "utf-8")
+  write.csv2(output$espec, file = paste0(file,"_especifications.csv"), row.names = F, na = "", fileEncoding = "utf-8")
   
 }
