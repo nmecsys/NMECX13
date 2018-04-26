12224687b18c59dcf10426cb8d973d0685d89f39
daianemarcolino <daimmattos@gmail.com>
2018-04-26 14:17:44 -0300

    update

--- R/saveX13.R
+++ R/saveX13.R
@@ -1,20 +1,28 @@
 #' @title Save seasonal adjustment results in multiple files
-#' @description Save CSV files for seasonally adjusted data, seasonal factors, calendar factors, total factors (seasonal & calendar factors) and model specification for each series.
+#' @description Export CSV files for seasonally adjusted data, seasonal factors, calendar factors, total factors (seasonal & calendar factors) and model specification for each series.
 #' @param output output from seasX13 function
 #' @param file a character string naming the file
+#' @examples 
+#' \dontrun{
+#' # read data example, auto seasonal adjustment, export results
+#' data(serviceSurvey)
+#' data <- readX13(serviceSurvey)
+#' auto <- seasX13(data)
+#' 
+#' saveX13(auto, "auto")
+#' }
 #' @importFrom zoo as.Date
 #' @export
  
 saveX13 <- function(output, file = ""){
   
-  if(file == ""){ stop("Insert the file name. No extensions are required.")}
+  if(file == ""){ stop("Insert the file name. No extensions are required.") }
 
   ajustadas <- data.frame(date = as.Date(output$xSA), output$xSA)
   fatores_totais <- data.frame(date = as.Date(output$totalFactors), output$totalFactors)
   fatores_sazonais <- data.frame(date = as.Date(output$seasonalFactors), output$seasonalFactors)
   fatores_calendario <- data.frame(date = as.Date(output$calendarFactors), output$calendarFactors)
   
-  # salvar serie com ajuste sazonal
   write.csv2(ajustadas, file = paste0(file,"_seasonallyAdjusted.csv"), row.names = F, na = "", fileEncoding = "utf-8")
   write.csv2(fatores_totais, file = paste0(file,"_totalFactors.csv"), row.names = F, na = "", fileEncoding = "utf-8")
   write.csv2(fatores_calendario, file = paste0(file,"_calendarFactors.csv"), row.names = F, na = "", fileEncoding = "utf-8")
