d3fceb767a2b01bf60c6cf2f6d98e1eebe6f2646
daianemarcolino <daimmattos@gmail.com>
2018-04-26 14:24:09 -0300

    services confidence index

---
+++ R/serviceSurvey.R
@@ -0,0 +1,8 @@
+#' @name  serviceSurvey
+#' @title Confidence index from Service Survey (FGV IBRE)
+#' @docType data
+#' @format  A \code{data.frame} with 4 columns: the first one is in \code{Date} format, the others are in \code{numeric}.    
+#' @description file example accepted in the seasonal adjustment routine. 
+#' @usage serviceSurvey
+#' @source The Dataset was collected from Service Survey (Getulio Vargas Foundation - Brazilian Institute of Economics).
+NULL
